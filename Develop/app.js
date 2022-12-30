71d0e7d0e30aaff40e3b07af0b785de12d26aab9
khinoj <kristopher.hinojosa@gmail.com>
2022-12-29 23:07:08 -0500

    update function

--- Develop/app.js
+++ Develop/app.js
@@ -14,51 +14,68 @@ const employees = [];
 
 
 // Write code to use inquirer to gather information about the development team members, and to create objects for each team member (using the correct classes as blueprints!)
-inquirer
-  .prompt()([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?',
-    },
-    {
+const employeeQuestion = () => {
+
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+      },
+      {
         type: 'input',
         name: 'id',
         message: 'What is your id number?',
-    },
-    {
+      },
+      {
         type: 'input',
         name: 'email',
         message: 'What is your email?',
-    },
-    {
+      },
+      {
         type: 'input',
         name: 'github',
         message: 'What is your GitHub username?',
-    },
-    {
+      },
+      {
         type: 'list',
         name: 'Role',
-        Choose: [
-            'Engineer',
-            'Intern',
-            'Manager',
+        choices: [
+          'Engineer',
+          'Intern',
+          'Manager',
         ],
         message: 'What is your role?',
 
-    },
-  ])
+      },
+    ])
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on employee type.
+    // HINT: each employee type (manager, engineer, or intern) has slightly different
+    // information; write your code to ask different questions via inquirer depending on employee type.
 
-  .then((answers) => {
-    // Use user feedback for... whatever!! 
-    let newEmployee;
-    if(answers.role === 'Engineer') {
+    .then((answers) => {
+      // Use user feedback for... whatever!! 
+      let newEmployee;
+      if (answers.role === 'Engineer') {
         newEmployee = new Engineer(answer.name, answer.id, answer.email, answer.github)
-    };
-  })
+      };
+      if (answer.role === 'Manager') {
+        newEmployee = new Engineer(answer.name, answer.id, answer.email, answer.officeNumber)
+      };
+      if (answer.role === 'Intern') {
+        newEmployee = new Intern(answer.name, answer.id, answer.email, answer.school)
+      }
+    })
+
+};
+
+const init = () => {
+  console.log('Please Answer the Following Questions');
+  employeeQuestion();
+};
+
+init();
 
 
 // After the user has input all employees desired, call the `render` function (required above) and pass in an array containing all employee objects; the `render` function will generate and return a block of HTML including templated divs for each employee!
@@ -67,10 +84,5 @@ inquirer
 // After you have your html, you're now ready to create an HTML file using the HTML returned from the `render` function. Now write it to a file named `team.html` in the `output` folder. You can use the variable `outputPath` above target this location.
 
 
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-                        /* Made and output folder */
-
-
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer, and Intern classes should all extend from a class named Employee; see the directions for further information. Be sure to test out each class and verify it generates an object with the correct structure and methods. This structure will be crucial in order for the provided `render` function to work!
+
